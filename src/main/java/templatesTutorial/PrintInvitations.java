af5e2a819c85be86433a15213ca13ea93988716c
Yolande <yolande.y.kok@gmail.com>
2020-09-25 22:40:32 +1200

    Added vm file

--- src/main/java/templatesTutorial/PrintInvitations.java
+++ src/main/java/templatesTutorial/PrintInvitations.java
@@ -4,48 +4,62 @@ import java.beans.XMLDecoder;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
 public class PrintInvitations {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		//List<Person> friendsList = new ArrayList<Person>();
+		List<Person> list = readFromXMLFileFunction();
 		
-		readFromXMLFile();
-		createVelocityfile();
-	
-	}
-	
-	
-	private static void createVelocityfile() {
-		
-		
+		createVelocityfile(list);
+
 	}
 
-	static void readFromXMLFile() {
+	private static void createVelocityfile(List<Person> list) throws IOException {
+		
+		//FileWriter out = new FileWriter("invitations.txt");
+		
+				VelocityContext context = new VelocityContext();
+		
+				context.put("friendList",list);
+				
+				Template template = Velocity.getTemplate("letter.vm");
+				
+				FileWriter out = new FileWriter("invitations.txt");
+				
+				template.merge(context, out);
+				
+				out.close();
+		 
+	}
+
+	static  List<Person> readFromXMLFileFunction() {
 
 		// Read from xmlfile into a list:
 		try {
-		
-		FileInputStream fileInput = new FileInputStream(new File("friends.xml"));
-		XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(fileInput));
 
-		List<Person> friendsList = (List<Person>) decoder.readObject(); // cast object read from file into person
-		decoder.close();
-		fileInput.close();
-		
-		for(Person friend: friendsList) {
-			System.out.println(friend);
-		}
+			FileInputStream fileInput = new FileInputStream(new File("friends.xml"));
+			XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(fileInput));
+
+			List<Person> friendsList = (List<Person>) decoder.readObject(); // cast object read from file into person
+			decoder.close();
+			fileInput.close();
+			return friendsList;
 
 		} catch (IOException ex) {
-		ex.printStackTrace();
+			ex.printStackTrace();
+
+		}
+		return null;
 
 	}
 
 }
-		
-	}
-
-
